5d11e2c1fd7fe139b57f96a44cef9205da444ea3
jack <jackzhoumine@gmail.com>
2021-06-04 00:46:16 +0800

    feat: 添加链接

--- src/examples/DebounceTest.vue
+++ src/examples/DebounceTest.vue
@@ -2,7 +2,7 @@
  * @Description: 防抖和节流
  * @Date: 2021-06-03 19:55:14 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-06-04 00:25:37 +0800
+ * @LastEditTime: 2021-06-04 00:45:33 +0800
  * @LastEditors: JackChou
 -->
 <template>
@@ -14,6 +14,7 @@
     <h2>节流---固定频率执行----关注过程变化过程是否平滑</h2>
     <el-button type="primary" @click="throttleClickDelay">延迟执行</el-button>
     <el-button type="primary" @click="throttleClickImmediate">立即执行</el-button>
+    <a href="http://demo.nimius.net/debounce_throttle/">该网站可观看目标函数执行频率</a>
   </div>
 </template>
 
