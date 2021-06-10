8a08ff9919c83f536a0ad5afa00ba51cead47e32
jack <jackzhoumine@gmail.com>
2021-06-10 19:33:13 +0800

    feat: 实现哈希表

--- src/data-structure/index.js
+++ src/data-structure/index.js
@@ -2,7 +2,7 @@
  * @Description:数据结构
  * @Date: 2020-05-17 18:17:23
  * @Author: JackChouMine
- * @LastEditTime: 2021-06-10 16:56:11 +0800
+ * @LastEditTime: 2021-06-10 18:52:24 +0800
  * @LastEditors: JackChou
  */
 import { Stack as stack } from './stack'
@@ -11,7 +11,7 @@ import { PriorityQueue as priorityQueue } from './priorityQueue'
 import { LinkList as linkList } from './linkList'
 import { DoubleLinkList as doubleLinkList } from './doubleLinkList'
 import { MySet as mySet } from './MySet'
-import { hashFunction as _hashFunction } from './hashMap'
+import { hashFunction as _hashFunction, HashTable as _hashTable } from './hashTable'
 export const Stack = stack
 export const Queue = queue
 export const PriorityQueue = priorityQueue
@@ -19,3 +19,4 @@ export const LinkList = linkList
 export const DoubleLinkList = doubleLinkList
 export const MySet = mySet
 export const hashFunction = _hashFunction
+export const HashTable = _hashTable
